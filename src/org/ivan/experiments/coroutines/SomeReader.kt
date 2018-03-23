5fc46107898dc089297765572ca02ebb915fb0c7
pavliv <ivan.pavluhin@f-secure.com>
2018-03-23 17:24:21 +0300

    SuspendingFillableByteSource with suspend method for reading byte by byte and SomeReader demo class

---
+++ src/org/ivan/experiments/coroutines/SomeReader.kt
@@ -0,0 +1,52 @@
+package org.ivan.experiments.coroutines
+
+import java.io.ByteArrayOutputStream
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.EmptyCoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+
+
+class SomeReader {
+  val source = SuspendingFillableByteSource()
+  private val fRead: suspend SuspendingFillableByteSource.() -> Int = { read() }
+  private val buf = arrayListOf<Int>()
+  private var readSomeBytes = false
+  private val cont = object : Continuation<Int> {
+    override val context = EmptyCoroutineContext
+
+    override fun resume(value: Int) {
+      buf.add(value)
+      readSomeBytes = true
+    }
+
+    override fun resumeWithException(exception: Throwable) = throw exception
+  }
+
+  fun readAvailable(): ByteArray {
+    do {
+      readSomeBytes = false
+      fRead.startCoroutine(source, cont)
+    } while (readSomeBytes)
+    val baos = ByteArrayOutputStream()
+    buf.forEach(baos::write)
+    buf.clear()
+    return baos.toByteArray()
+  }
+}
+
+fun main(args: Array<String>) {
+  println("Coroutine demo")
+  val reader = SomeReader()
+  reader.readAvailable().printHex()
+  reader.source.addBytes(byteArrayOf(1, 2, 3))
+  reader.readAvailable().printHex()
+  reader.readAvailable().printHex()
+}
+
+private fun ByteArray.printHex() {
+  val sb = StringBuilder()
+  forEach {
+    sb.append(Integer.toHexString(0xFF and it.toInt()))
+  }
+  println(sb.toString())
+}
---
+++ src/org/ivan/experiments/coroutines/SuspendingFillableByteSource.kt
@@ -0,0 +1,46 @@
+package org.ivan.experiments.coroutines
+
+import java.util.*
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.suspendCoroutine
+
+
+class SuspendingFillableByteSource {
+  private val bytes = LinkedList<ByteArray>()
+  private var readerIndex = 0
+  private var end = false
+  var continuation: Continuation<Int>? = null
+
+  private fun isReadable() = bytes.isNotEmpty() && readerIndex < bytes.first.size
+
+  suspend fun read(): Int {
+    if (isReadable()) {
+      return read0()
+    }
+    if (end) {
+      return -1
+    }
+    return suspendCoroutine { cont ->
+      continuation = cont
+    }
+  }
+
+  private fun read0(): Int {
+    val chunk = bytes.first
+    val ret = chunk[readerIndex++].toInt()
+    if (readerIndex == chunk.size) bytes.removeFirst()
+    return ret
+  }
+
+  fun addBytes(b: ByteArray) {
+    if (b.isNotEmpty()) {
+      bytes.add(b)
+      continuation?.resume(read0())
+    }
+  }
+
+  fun end() {
+    end = true
+    continuation?.resume(-1)
+  }
+}
